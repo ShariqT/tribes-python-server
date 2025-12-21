086602c539b53c0848054d491ff12e81b23ba4c0
Shariq Torres <shariq.torres@gmail.com>
2025-12-20 21:19:44 -0800

    added more info for server command

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ parser.add_argument("--setup-db", action='store_true')
 parser.add_argument("--add-superuser")
 parser.add_argument('--run-dev-server', action='store_true')
 parser.add_argument('--run-server', action='store_true')
-parser.add_argument('--port', type=int, default=8080)
+# parser.add_argument('--port', type=int, default=8080)
 
 args = parser.parse_args()
 r = redis.Redis(host=os.environ['REDIS_HOST'], 
@@ -53,33 +53,27 @@ if args.setup_db is True:
   r.close()
 
 if args.add_superuser is not None:
-  keyfile = open(args.add_superuser)
+  keyfile = open(os.environ['ADMIN_KEY'])
   keydata = keyfile.read()
   superuser_key = garden.create_key_from_text(keydata)
   name_id = garden.generate_key_name_id(superuser_key)
-  # superuser_key_users = superuser_key.userids
-  # print(superuser_key.fingerprint[-8:])
-  # name_id = superuser_key_users[0].name + "-" + superuser_key.fingerprint[-8:]
   r.set("superuser_username", name_id)
   r.set("superuser", keydata)
   print(f"Saved public key in {args.add_superuser} to the superuser of this server")
   r.close()
   exit()
 
-# TODO: Add the dotenv package to get the env variables for
-# the databse logins/host and the host and ports to serve the flask app on
-# also put in the production version of this call which will call waitress, 
-# which is already installed. 
+
 if args.run_dev_server is True:
   os.environ['MODE'] = 'DEBUG'
-  subprocess.call(f"flask --app server_src --debug run --port {args.port}", shell=True)
+  subprocess.call(f"flask --app server_src --debug run --port {os.environ['PORT']}", shell=True)
 
 if args.run_server is True:
   from waitress import serve
   from server_src import app
   os.environ['MODE'] = 'PROD'
-  print(f"Running production server on port {args.port}")
-  serve(app, host='0.0.0.0', port=args.port)
+  print(f"Running production server on port {os.environ['PORT']}")
+  serve(app, host='0.0.0.0', port=os.environ['PORT'])
 
 
 
